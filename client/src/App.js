56c5ef6c2a7d6cdd1d84f83f35a982865d8608cb
quang huy <damquanghuy1245@gmail.com>
2023-05-10 14:26:36 +0700

    first push

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,61 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import { Outlet } from "react-router-dom";
+
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom"
+import Home from './pages/Home/Home';
+import Category from './pages/Category/Category';
+import Products from './pages/Products/Products';
+import Navbar from './Component/Navbar/Navbar';
+import Footer from './Component/Footers/Footer';
+import "./App.scss"
+import Login from "./Component/Login/Login";
+import Register from "./Component/register/Register";
+
+
+//outlet  hiển thị các component con tương ứng với các route khác nhau trong ứng dụng.
+const Layout = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: "/Category",
+          element: <Category />,
+        },
+        {
+          path: "/Products",
+          element: <Products />,
+        },
+        {
+          path: "/Login",
+          element: <Login />
+        },
+        {
+          path: "/Register",
+          element: <Register />
+        }
+      ],
+    },
+
+  ]);
+
   return (
-    <div className="App">
-      <Navbar/>
-      <Header/>
-      <Outlet/>
-      <Footer/>
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
-
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js"
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Products from "./pages/Products";
-import Product from "./pages/Product";
-import Home from "./pages/Home";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="products" element={<Products />} />
-          <Route path="product" element={<Product />} />
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
