a5e50784e2c1de091dd25cf09f0bd1d635b42ea2
Huu An <nguyenhuuan1304@gmail.com>
2023-05-15 10:42:40 +0700

    fix...

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,6 +5,7 @@ function Header() {
               <div className="text-center text-white">
                   <h1 className="display-4 fw-bolder">Shop in style</h1>
                   <p className="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+                  <p className="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
               </div>
           </div>
       </header>
