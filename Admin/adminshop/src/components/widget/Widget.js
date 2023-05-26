0bd7a8d8c7819d98a50aabe3c41d7ed1e9eb600e
quang huy <damquanghuy1245@gmail.com>
2023-05-26 09:14:40 +0700

    update

--- Admin/adminshop/src/components/widget/Widget.js
+++ Admin/adminshop/src/components/widget/Widget.js
@@ -1,19 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Widget.scss"
+import { useEffect } from 'react';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import PersonIcon from '@mui/icons-material/Person';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-import PaymentRoundedIcon from '@mui/icons-material/PaymentRounded';
+import Inventory2Icon from '@mui/icons-material/Inventory2';
 const Widget = ({ type }) => {
+    const [amountuser, setAmountUser] = useState(0)
+    const [amountOrder, setAmounOrder] = useState(0)
+    const [amountproduct, setAmounProduct] = useState(0)
     let data;
-    const amount = 100
     const diff = 20
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // Gọi API lần 1
+                const response1 = await fetch('http://localhost:1337/api/admins');
+                const data1 = await response1.json();
+
+                // Lấy dữ liệu từ response1 và gọi API lần 2
+                const response2 = await fetch(`http://localhost:1337/api/orders`);
+                const data2 = await response2.json();
+
+
+                const response3 = await fetch(`http://localhost:1337/api/products`)
+                const data3 = await response3.json()
+                // Lưu dữ liệu từ response vào state
+                setAmountUser(data1)
+                setAmounOrder(data2);
+                setAmounProduct(data3)
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+    console.log(amountproduct)
 
     switch (type) {
         case "user":
             data = {
                 title: "USER",
+                amount: amountuser?.data?.length,
                 isMoney: false,
                 link: "See all users",
                 icon: <PersonIcon className='icon' />,
@@ -22,17 +52,19 @@ const Widget = ({ type }) => {
         case "order":
             data = {
                 title: "ORDER",
+                amount: amountOrder?.data?.length,
                 isMoney: false,
                 link: "view all order",
                 icon: <ShoppingBasketIcon className='icon' />,
             };
             break;
-        case "earning":
+        case "product":
             data = {
-                title: "EARNINGS",
+                title: "PRODUCT",
+                amount: amountproduct?.data?.length,
                 isMoney: false,
-                link: "view net earnings",
-                icon: <PaymentRoundedIcon className='icon' />,
+                link: "view net Product",
+                icon: <Inventory2Icon className='icon' />,
             };
             break;
         default:
@@ -43,7 +75,7 @@ const Widget = ({ type }) => {
         <div className='widget'>
             <div className='left'>
                 <span className='title'>{data.title}</span>
-                <span className='counter'>{data.isMoney && "$"}{amount} </span>
+                <span className='counter'>{data.isMoney && "$"}{data.amount} </span>
                 <span className='link'>{data.link}</span>
             </div>
             <div className='right'>
